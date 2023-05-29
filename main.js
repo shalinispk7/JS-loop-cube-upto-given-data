85c82d4ea747810c79dfd111f6d32405a560d2f5
shalinispk7 <shalinispk7@gmail.com>
2023-05-29 22:21:34 +0530

    loop-cube of the upto given data

---
+++ main.js
@@ -0,0 +1,6 @@
+// number and their cube upto given data
+let n=parseInt(prompt("Enter number"));
+for(i=1;i<=n;i++){
+	 document.write("Number is: "+i+" and Cube of " +i+ " is " + i*i*i);
+	 document.write("<br>"); 
+}
